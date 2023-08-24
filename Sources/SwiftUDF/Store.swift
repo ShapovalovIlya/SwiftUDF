d9698cbb09eb2edfb8d92f3550c66fa03953e5c5
Ilia Shapovalov <greateman@mail.ru>
2023-08-25 02:02:11 +0300

    Add logger settings

--- Sources/SwiftUDF/Store.swift
+++ Sources/SwiftUDF/Store.swift
@@ -15,24 +15,36 @@ public typealias StoreOf<R: ReducerDomain> = Store<R.State, R.Action>
 public final class Store<State, Action>: ObservableObject {
     private let logger = Logger(
         subsystem: Bundle.main.bundleIdentifier!,
-        category: "Store<\(State.self)>"
+        category: "StoreOf<\(State.self)>"
     )
     private let reducer: any ReducerDomain<State, Action>
     private var cancellable: Set<AnyCancellable> = .init()
+    private let logExhaustive: LogExhaustive
     
     @Published public private(set) var state: State
     
     //MARK: - init(_:)
     public init<R: ReducerDomain>(
         state: R.State,
-        reducer: R
+        reducer: R,
+        logExhaustive: LogExhaustive = .none
     ) where R.State == State, R.Action == Action {
         self.state = state
         self.reducer = reducer
+        self.logExhaustive = logExhaustive
+        
+        switch logExhaustive {
+        case .all, .state: logState()
+        default: break
+        }
     }
     
     //MARK: - Public methods
     public func send(_ action: Action) {
+        switch logExhaustive {
+        case .all, .action: logger.debug("\(String(describing: action))")
+        default: break
+        }
         reducer.reduce(&state, action: action)
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: send)
@@ -46,13 +58,26 @@ public final class Store<State, Action>: ObservableObject {
     public subscript<T>(dynamicMember keyPath: KeyPath<State, T>) -> T {
         state[keyPath: keyPath]
     }
+}
+
+private extension Store {
+    func log(content: String) {
+        logger.debug("\(content)")
+    }
     
-    public func logState() -> Self {
+    func logState() {
         self.$state
-            .map(String.init(reflecting:))
-            .sink { [weak self] in self?.logger.debug("\($0)") }
-            .store(in: &self.cancellable)
-        
-        return self
+            .map(String.init(describing:))
+            .sink(receiveValue: log(content:))
+            .store(in: &cancellable)
+    }
+}
+
+extension Store {
+    public enum LogExhaustive {
+        case none
+        case all
+        case state
+        case action
     }
 }
